e9f9070d5b179cc279409f8a5fd411d34590eb89
Your Name <you@example.com>
2019-03-30 14:56:33 -0500

    c

--- src/pages/detallesucursal/detallesucursal.ts
+++ src/pages/detallesucursal/detallesucursal.ts
@@ -1,8 +1,8 @@
-import * as HighCharts from 'highcharts';
 
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angular';
 
+import * as HighCharts from 'highcharts';
 
 /**
  * Generated class for the DetallesucursalPage page.
@@ -31,14 +31,14 @@ export class DetallesucursalPage {
   ionViewDidLoad() {
     console.log('ionViewDidLoad DetallesucursalPage');
 
-    Highcharts.chart('container', {
+var myChart = Highcharts.chart('container', {
 
     title: {
-        text: 'Solar Employment Growth by Sector, 2010-2016'
+        text: 'Ventas por Mes'
     },
 
     subtitle: {
-        text: 'Source: thesolarfoundation.com'
+        text: 'Source: xiencias.com'
     },
 
     yAxis: {
@@ -96,7 +96,76 @@ export class DetallesucursalPage {
 });
 
 
+var myChart1 = Highcharts.chart('container1', {
+
+    title: {
+        text: 'Ventas por Dia'
+    },
+
+    subtitle: {
+        text: 'Source: xiencias.com'
+    },
+
+    yAxis: {
+        title: {
+            text: 'Number of Employees'
+        }
+    },
+    legend: {
+        layout: 'vertical',
+        align: 'right',
+        verticalAlign: 'middle'
+    },
+
+    plotOptions: {
+        series: {
+            label: {
+                connectorAllowed: false
+            },
+            pointStart: 2010
+        }
+    },
+
+    series: [{
+        name: 'Installation',
+        data: [43934, 52503, 57177, 69658, 97031, 119931, 137133, 154175]
+    }, {
+        name: 'Manufacturing',
+        data: [24916, 24064, 29742, 29851, 32490, 30282, 38121, 40434]
+    }, {
+        name: 'Sales & Distribution',
+        data: [11744, 17722, 16005, 19771, 20185, 24377, 32147, 39387]
+    }, {
+        name: 'Project Development',
+        data: [null, null, 7988, 12169, 15112, 22452, 34400, 34227]
+    }, {
+        name: 'Other',
+        data: [12908, 5948, 8105, 11248, 8989, 11816, 18274, 18111]
+    }],
+
+    responsive: {
+        rules: [{
+            condition: {
+                maxWidth: 500
+            },
+            chartOptions: {
+                legend: {
+                    layout: 'horizontal',
+                    align: 'center',
+                    verticalAlign: 'bottom'
+                }
+            }
+        }]
+    }
+
+});
+
+
+
   }
+
+
+
 
   closeModal(){
 
